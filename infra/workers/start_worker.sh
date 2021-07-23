2527a1e8dfbd90035beea46acf133d87d3c7ef53
Spencer Nelson <swnelson@uw.edu>
2021-07-23 16:45:43 -0700

    Start worker under thorctl name

--- infra/workers/start_worker.sh
+++ infra/workers/start_worker.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
-
 source /opt/miniconda3/etc/profile.d/conda.sh
 conda activate thor_py38
 
-source /etc/thor/env
-
-python /opt/thor/runTHORWorker.py \
-       --idle-shutdown-timeout=60 \
-       ${THOR_QUEUE}
+exec run-thor-worker --idle-shutdown-timeout=60 ${THOR_QUEUE}
