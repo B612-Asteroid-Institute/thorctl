b45bee4aac82c7d92a049fec4fdbd3da016c00a8
Spencer Nelson <swnelson@uw.edu>
2021-08-13 16:43:21 -0700

    Correctly invoke update script

--- infra/build.sh
+++ infra/build.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "${SCRIPT_DIR}/../"
 
-docker build -t thorctl .
+docker build -t thorctl:production-tasks-latest .
--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,6 +5,8 @@ set -xeuo pipefail
 docker tag thorctl:production-tasks-latest gcr.io/moeyens-thor-dev/thorctl:production-tasks-latest
 docker push gcr.io/moeyens-thor-dev/thorctl:production-tasks-latest
 
-cd autoscaler
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR/autoscaler
 ./update.sh
 cd -
