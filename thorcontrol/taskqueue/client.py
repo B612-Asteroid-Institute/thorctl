48859b97f11d98adf880408693bfc6c55885b3aa
Spencer Nelson <swnelson@uw.edu>
2021-08-19 16:25:49 -0700

    Bump up timeout and add a note

--- thorcontrol/taskqueue/client.py
+++ thorcontrol/taskqueue/client.py
@@ -373,7 +373,8 @@ class Worker:
             logger.info(
                 "beginning execution for job %s, task %s", task.job_id, task.task_id
             )
-            asyncio.run(run_thor_subprocess(input_dir, output_dir, timeout=60))
+            # TODO: Make this timeout a parameter on the task, or maybe a deadline associated with the Job?
+            asyncio.run(run_thor_subprocess(input_dir, output_dir, timeout=60 * 60 * 4))
             self.mark_task_succeeded(task, bucket, output_dir)
 
         except Exception as e:
